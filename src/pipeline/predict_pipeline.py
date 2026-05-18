6e6e32e7c23ddc24696734716462771f50d2c0c8
ANAY NANDI <anaynandi2017@gmail.com>
2026-05-18 18:40:48 +0530

    fixed path issue

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -2,7 +2,7 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
-
+import os
 
 class PredictPipeline:
     def __init__(self):
@@ -10,8 +10,8 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path = 'artifact\model.pkl'
-            preprocessor_path = 'artifact\preprocessor.pkl'
+            model_path = os.path.join("artifact", "model.pkl")
+            preprocessor_path = os.path.join("artifact", "preprocessor.pkl")
             model = load_object(file_path = model_path)
             preprocessor = load_object(file_path = preprocessor_path)
             data_scaled = preprocessor.transform(features)
